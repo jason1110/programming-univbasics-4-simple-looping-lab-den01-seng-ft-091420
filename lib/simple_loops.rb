c8e575719de141ba6e04574369c331b75c6910f8
Jason Easterly <jason.easterly11@gmail.com>
2020-08-18 00:36:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,3 +13,13 @@ def loop_message_n_times string , n
     counter += 1
   end
 end
+
+def output_array array
+  counter = 0
+  while counter < array.length do
+    puts array[counter]
+    counter +=1
+  end
+end
+
+def 
